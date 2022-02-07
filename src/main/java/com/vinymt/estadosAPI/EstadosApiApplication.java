f94c87f9525f0f615b814b75c662d7922eea081e
VinyMT <viniciusmatiolatramontin@gmail.com>
2022-02-07 13:38:04 -0300

    Project created

---
+++ src/main/java/com/vinymt/estadosAPI/EstadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinymt.estadosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinymt/estadosAPI/EstadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinymt.estadosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
